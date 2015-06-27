77594216e9eb3811cab1420d9e21fe6c55337896
Spencer Krieger <gkrieg13@gmail.com>
2015-06-27 11:41:06 -0600

    added constructor to player

--- java/src/shared/model/Player.java
+++ java/src/shared/model/Player.java
@@ -13,5 +13,21 @@ public class Player {
 	private Name name;
 	private TradeRatios tradeRatios;
 	private PlayerID playerID;
+	
+	public Player(Color color) {
+		this.playerBank = new PlayerBank();
+		this.longestRoad = new LongestRoad();
+		this.largestArmy = new LargestArmy();
+		this.color = color;
+		this.roads = new Roads();
+		this.settlements = new Settlements();
+		this.cities = new Cities();
+		this.victoryPoints = new VictoryPoints();
+		this.name = new Name();
+		this.tradeRatios = new TradeRatios();
+		this.playerID = new PlayerID();
+	}
+	
+	
 
 }
