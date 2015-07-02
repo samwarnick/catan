bb5fe706f421d15194848bea5cbc9dd785ac3c54
Spencer Krieger <gkrieg13@gmail.com>
2015-07-02 09:19:46 -0600

    finished the proxy server

--- src/client/proxy/ProxyServer.java
+++ src/client/proxy/ProxyServer.java
@@ -9,190 +9,199 @@ import shared.model.GameModel;
 
 public class ProxyServer implements IServer {
 
+	ClientCommunicator clientCommunicator;
+	
+	public ProxyServer(ClientCommunicator clientCommunicator) {
+		super();
+		this.clientCommunicator = clientCommunicator;
+	}
+
 	@Override
 	public String loginUser(UserLoginInput input) throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		String toReturn = (String) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public String registerUser(UserRegisterInput input) throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		String toReturn = (String) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public List<GameModel> listGames(GamesListInput input)
 			throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		@SuppressWarnings("unchecked")
+		List<GameModel> toReturn = (List<GameModel>) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public GameModel createGame(GamesCreateInput input) throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		GameModel toReturn = (GameModel) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public String joinGame(GamesJoinInput input) throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		String toReturn = (String) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public boolean saveGame(GamesSaveInput input) throws ServerException {
-		// TODO Auto-generated method stub
-		return false;
+		boolean toReturn = (boolean) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public GameModel loadGame(GamesLoadInput input) throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		GameModel toReturn = (GameModel) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public GameModel getGameModelVersion(GameModelVersionInput input)
 			throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		GameModel toReturn = (GameModel) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public GameModel resetGame(GameResetInput input) throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		GameModel toReturn = (GameModel) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public List<String> getGameCommands(GameCommandsGetInput input)
 			throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		@SuppressWarnings("unchecked")
+		List<String> toReturn = (List<String>) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public GameModel postGameCommands(GameCommandsPostInput input)
 			throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		GameModel toReturn = (GameModel) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public boolean changeLogLevel(UtilChangeLogLevelInput input)
 			throws ServerException {
-		// TODO Auto-generated method stub
-		return false;
+		boolean toReturn = (boolean) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public GameModel sendChat(SendChatInput input) throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		GameModel toReturn = (GameModel) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public GameModel acceptTrade(AcceptTradeInput input) throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		GameModel toReturn = (GameModel) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public GameModel discardCards(DiscardCardsInput input)
 			throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		GameModel toReturn = (GameModel) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public GameModel rollNumber(RollNumberInput input) throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		GameModel toReturn = (GameModel) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public GameModel buildRoad(BuildRoadInput input) throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		GameModel toReturn = (GameModel) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public GameModel buildSettlement(BuildSettlementInput input)
 			throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		GameModel toReturn = (GameModel) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public GameModel buildCity(BuildCityInput input) throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		GameModel toReturn = (GameModel) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public GameModel offerTrade(OfferTradeInput input) throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		GameModel toReturn = (GameModel) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public GameModel maritimeTrade(MaritimeTradeInput input)
 			throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		GameModel toReturn = (GameModel) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public GameModel robPlayer(RobPlayerInput input) throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		GameModel toReturn = (GameModel) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public GameModel finishTurn(FinishTurnInput input) throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		GameModel toReturn = (GameModel) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public GameModel buyDevCard(BuyDevCardInput input) throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		GameModel toReturn = (GameModel) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public GameModel playSoldier(PlaySoldierInput input) throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		GameModel toReturn = (GameModel) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public GameModel playYearOfPlenty(PlayYearOfPlentyInput input)
 			throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		GameModel toReturn = (GameModel) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public GameModel playRoadBuilding(PlayRoadBuildingInput input)
 			throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		GameModel toReturn = (GameModel) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public GameModel playMonopoly(PlayMonopolyInput input)
 			throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		GameModel toReturn = (GameModel) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	@Override
 	public GameModel playMonument(PlayMonumentInput input)
 			throws ServerException {
-		// TODO Auto-generated method stub
-		return null;
+		GameModel toReturn = (GameModel) clientCommunicator.post(input);
+		return toReturn;
 	}
 
 	
