8f717dd9b83f6ececaf145f7aea2ac2292404bb8
ihartung <isaac.hartung@gmail.com>
2015-07-05 14:35:54 -0600

    Final_Bank

--- src/shared/model/bank/Bank.java
+++ src/shared/model/bank/Bank.java
@@ -2,6 +2,7 @@ package shared.model.bank;
 
 import java.util.ArrayList;
 
+import shared.communication.input.move.ResourceHand;
 import shared.definitions.DevCardType;
 import shared.definitions.ResourceType;
 
@@ -24,31 +25,19 @@ public class Bank {
 	private ResourceCard sheep;
 	private ResourceCard wheat;
 	private ResourceCard ore;
-	private DevelopmentCard soldier;
-	private DevelopmentCard yearOfPlenty;
-	private DevelopmentCard buildRoad;
-	private DevelopmentCard monopoly;
-	private DevelopmentCard monument;
+	protected DevelopmentCard soldier;
+	protected DevelopmentCard yearOfPlenty;
+	protected DevelopmentCard roadBuild;
+	protected DevelopmentCard monopoly;
+	protected DevelopmentCard monument;
 	private boolean largestArmyCard = false;
 	private boolean longestRoadCard = false;
 	
-	public ArrayList<ResourceCard> getResourceCards() {
-		return resourceCards;
-	}
-	public ArrayList<DevelopmentCard> getDevelopmentCards() {
-		return developmentCards;
-	}
 	public boolean hasLargestArmyCard() {
 		return largestArmyCard;
 	}
 	public boolean hasLongestRoadCard() {
 		return longestRoadCard;
-	}
-	public void setResourceCards(ArrayList<ResourceCard> resourceCards) {
-		this.resourceCards = resourceCards;
-	}
-	public void setDevelopmentCards(ArrayList<DevelopmentCard> developmentCards) {
-		this.developmentCards = developmentCards;
 	}
 	public void setLargestArmyCard(boolean largestArmyCard) {
 		this.largestArmyCard = largestArmyCard;
@@ -61,53 +50,44 @@ public class Bank {
 	 * adds a single resource card to the bank.
 	 * 
 	 * @param RC is a ResourceCard object
+	 * @throws Exception 
 	 * @pre RC is not null
 	 * @post RC is added to the Bank's Array
 	 * 
 	 */
 	
-	public void addRC(ResourceCard RC){
-		if(RC == null) return;
-		resourceCards.add(RC);
+	public void addRC(ResourceHand rh) throws Exception{
+		brick.modify(rh.getBrick());
+		wheat.modify(rh.getWheat());
+		ore.modify(rh.getOre());
+		wood.modify(rh.getWood());
+		sheep.modify(rh.getSheep());
 	}
 	
 	/**
 	 * adds a single development card to the bank.
 	 * 
 	 * @param DC is a DevelopmentCard Object
+	 * @throws Exception 
 	 * @pre DC is not null
 	 * @post DC is added to the Bank's Array of DC Cards
 	 */
 	
-	public void addDC(DevelopmentCard DC){
-		if(DC == null) return;
-		developmentCards.add(DC);
-	}
-	
-	/**
-	 * returns a resource card of the type specified by the 
-	 * parameter. if not found, it returns null
-	 * 
-	 * @param rt is a ResouceType
-	 * @pre rt is a valid ResourceType, namely Grain, Ore, etc.
-	 * @post a ResourceCard Object of the parameter type is returned, null if the bank has none.
-	 */
-	
-	public ResourceCard getRC(ResourceType rt){
-		return null;
-	}
-	
-	/**
-	 * returns a Development card of the type specified by the 
-	 * parameter. if not found, it returns null
-	 * 
-	 * @param dt is a DevCardType 
-	 * @pre dt is valid DevCardType, namely Soldier, Monopoly, etc.
-	 * @post a DevelopmentCard Object of the parameter type is returned, null if the bank has none.
-	 */
-	
-	public DevelopmentCard getDC(DevCardType dt){
-		return null;
+	public void addDC(DevCardType card) throws Exception{
+		switch(card){
+		case SOLDIER: soldier.modify(1);
+			break;
+		case MONOPOLY: monopoly.modify(1);
+			break;
+		case YEAR_OF_PLENTY: yearOfPlenty.modify(1);
+			break;
+		case ROAD_BUILD: roadBuild.modify(1);
+			break;
+		case MONUMENT: monument.modify(1);
+			break;
+		default:
+			throw new Exception("bad type parameter");
+		}
 	}
 	
 	/**
@@ -120,8 +100,13 @@ public class Bank {
 	 * @post returns true if the player has the specified number of the specified type, false otherwise. 
 	 */
 	
-	public boolean hasRC(ResourceType rt, int quantity){
-		return false;
+	public boolean hasRC(ResourceHand rh){
+		if(ore.getQuantity()<rh.getOre()) return false;
+		if(wood.getQuantity()<rh.getWood()) return false;
+		if(sheep.getQuantity()<rh.getSheep()) return false;
+		if(wheat.getQuantity()<rh.getWheat()) return false;
+		if(brick.getQuantity()<rh.getBrick()) return false;
+		return true;
 	}
 	
 	/**
@@ -130,13 +115,31 @@ public class Bank {
 	 * 
 	 * @param dt is a DevCardType
 	 * @param quantity is the number of cards we want to find
+	 * @throws Exception 
 	 * @pre a valid DevCardType and a non-negative quantity
 	 * @post returns true if the player has the specified number of the specified type, false otherwise.
 	 * 
 	 */
 	
-	public boolean hasDC(DevCardType dt, int quantity){
-		return false;
+	public boolean hasDC(DevCardType dt, int quantity) throws Exception{
+		int temp = 0;
+		switch(dt){
+		case SOLDIER: temp = soldier.getQuantity();
+			break;
+		case MONOPOLY: temp = monopoly.getQuantity();
+			break;
+		case YEAR_OF_PLENTY: yearOfPlenty.modify(1);
+			break;
+		case ROAD_BUILD: roadBuild.modify(1);
+			break;
+		case MONUMENT: monument.modify(1);
+			break;
+		default:
+			throw new Exception("bad type parameter");
+		}
+		
+		if(temp<quantity) return false;
+		return true;
 	}
 	
 	
