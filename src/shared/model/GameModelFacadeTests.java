d37d85e7e92f1b7bd015cbcccb491fb76284aec5
ihartung <isaac.hartung@gmail.com>
2015-07-05 17:52:55 -0600

    setting up JUnit tests

---
+++ src/shared/model/GameModelFacadeTests.java
@@ -0,0 +1,13 @@
+package shared.model;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GameModelFacadeTests {
+
+}
