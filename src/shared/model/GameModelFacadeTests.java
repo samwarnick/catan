a52b7f6170e24c925e9381b718863eb97bb98d60
ihartung <isaac.hartung@gmail.com>
2015-07-05 19:05:43 -0600

    more Junit setup

--- src/shared/model/GameModelFacadeTests.java
+++ src/shared/model/GameModelFacadeTests.java
@@ -8,13 +8,75 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import shared.definitions.CatanColor;
+import shared.model.player.Color;
 import shared.model.player.Player;
 
 public class GameModelFacadeTests {
 	
 	private Player player;
 	
-	@Before prep(){
+	@Before void prep(){
+		player = new Player(Color.RED, "George", 1);
+	}
+	
+	@Test
+	public void testCanAcceptTrade(){
+		
+	}
+	
+	@Test
+	public void testCanDiscardCards(){
+		
+	}
+	
+	@Test
+	public void testCanRollDice(){
+		
+	}
+	
+	@Test
+	public void testCanBuildRoad(){
+		
+	}
+	
+	@Test
+	public void testCanBuildSettlement(){
+		
+	}
+	
+	@Test
+	public void testCanBuildCity(){
+		
+	}
+	
+	@Test
+	public void testCanOfferTrade(){
+		
+	}
+	
+	@Test
+	public void testCanMaritimeTrade(){
+		
+	}
+	
+	@Test
+	public void testCanRobPlayer(){
+		
+	}
+	
+	@Test
+	public void testCanFinishTurn(){
+		
+	}
+	
+	@Test
+	public void testCanBuyDevCard(){
+		
+	}
+	
+	@Test
+	public void testCanPlayDevCard(){
 		
 	}
 
