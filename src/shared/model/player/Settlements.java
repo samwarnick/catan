16ccfcedfddd126aee14704e788dc3c737067a11
Spencer Krieger <gkrieg13@gmail.com>
2015-07-06 09:20:29 -0600

    fixed the settlement and road hopefully

--- src/shared/model/player/Settlements.java
+++ src/shared/model/player/Settlements.java
@@ -31,12 +31,22 @@ public class Settlements {
 	 * @pre none
 	 * @param settlement
 	 * @throws NoSettlementsLeftException
+	 * @throws SettlementAlreadyThereException 
 	 * @post adds a Settlement, or throws NoSettlementsLeftException.
 	 */
-	public void buildSettlement(Settlement settlement) throws NoSettlementsLeftException {
+	public void buildSettlement(Settlement settlement) throws NoSettlementsLeftException, SettlementAlreadyThereException {
 		if(settlementsLeft > 0){
-			settlements.add(settlement);
-			settlementsLeft--;
+			boolean alreadythere = false;
+			for (Settlement s : settlements){
+				if (s.getLocation().equals(settlement.getLocation()))
+					alreadythere = true;
+			}
+			if (!alreadythere){
+				settlements.add(settlement);
+				settlementsLeft--;
+			}
+			else
+				throw new SettlementAlreadyThereException();
 		}
 		else
 			throw new NoSettlementsLeftException();
@@ -49,11 +59,22 @@ public class Settlements {
 	/**
 	 * @pre settlement(passed in as a parameter) must have been previously added to Settlements
 	 * @param settlement
+	 * @throws NoSettlementFoundException 
 	 * @post removes the Settlement specified in the parameter.
 	 */
-	public void subtractSettlement(Settlement settlement){
-		settlements.remove(settlement);
-		settlementsLeft++;
+	public void subtractSettlement(Settlement settlement) throws NoSettlementFoundException{
+		boolean removed = false;
+		for (int i = 0;i < settlements.size();i++){
+			if (settlements.get(i).getLocation().equals(settlement.getLocation()) && !removed){
+				settlements.remove(i);
+				settlementsLeft++;
+				removed = true;
+			}
+		}
+		if(removed){
+			throw new NoSettlementFoundException();
+		}
+		
 	}
 	
 	
@@ -62,5 +83,13 @@ public class Settlements {
 }
 @SuppressWarnings("serial")
 class NoSettlementsLeftException extends Exception{
+	
+}
+@SuppressWarnings("serial")
+class NoSettlementFoundException extends Exception{
+	
+}
+@SuppressWarnings("serial")
+class SettlementAlreadyThereException extends Exception{
 	
 }
