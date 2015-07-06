47478b4cd798185c3dcd96dc503e5467491b2676
samwarnick <sam.t.warnick@gmail.com>
2015-07-06 10:38:44 -0600

    Add player package tests
    changed Settlements subtract and added tests

--- src/shared/model/board/PlayerID.java
+++ src/shared/model/board/PlayerID.java
@@ -17,5 +17,26 @@ public class PlayerID {
 		if (num >= -1 && num <= 3)
 			this.playerid = num;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + playerid;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PlayerID other = (PlayerID) obj;
+		if (playerid != other.playerid)
+			return false;
+		return true;
+	}
 }
--- src/shared/model/board/Vertex.java
+++ src/shared/model/board/Vertex.java
@@ -27,5 +27,34 @@ public class Vertex {
 	public void setLocation(VertexLocation location) {
 		this.location = location;
 	}
-
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((location == null) ? 0 : location.hashCode());
+		result = prime * result + ((owner == null) ? 0 : owner.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vertex other = (Vertex) obj;
+		if (location == null) {
+			if (other.location != null)
+				return false;
+		} else if (!location.equals(other.location))
+			return false;
+		if (owner == null) {
+			if (other.owner != null)
+				return false;
+		} else if (!owner.equals(other.owner))
+			return false;
+		return true;
+	}
 }
