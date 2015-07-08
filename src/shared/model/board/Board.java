3a20a476556a0a5495d05d978aa4e0178c7838c5
Matt0911 <Mattmanhardt@Gmail.com>
2015-07-08 10:16:39 -0600

    Update Board.java
    Fixed loops, should  generate correct number of landhexes

--- src/shared/model/board/Board.java
+++ src/shared/model/board/Board.java
@@ -148,7 +148,7 @@ public class Board {
 		
 		x = -1;
 		y = -2;
-		hexLoc = new HexLocation(x, y++);
+		hexLoc = new HexLocation(x, y);
 		vertex1 = new VertexLocation(hexLoc, VertexDirection.SouthWest);
 		vertex2 = new VertexLocation(hexLoc, VertexDirection.SouthEast);
 		orientation = new EdgeLocation(hexLoc, EdgeDirection.South);
@@ -192,7 +192,7 @@ public class Board {
 		
 		x = 1;
 		y = -3;
-		hexLoc = new HexLocation(x, y++);
+		hexLoc = new HexLocation(x, y);
 		vertex1 = new VertexLocation(hexLoc, VertexDirection.SouthWest);
 		vertex2 = new VertexLocation(hexLoc, VertexDirection.SouthEast);
 		orientation = new EdgeLocation(hexLoc, EdgeDirection.South);
@@ -249,10 +249,10 @@ public class Board {
 		ports.add(createPortHex(hexLoc, portIt.next(), vertex1, vertex2, orientation));
 		waterHexes.add(new WaterHex(new HexLocation(x, y)));
 		
-//		assert !landIt.hasNext();
+		assert !landIt.hasNext();
 		assert !portIt.hasNext();
-//		assert !numIt.hasNext();
-//		assert resourceHexes.size() == 18;
+		assert !numIt.hasNext();
+		assert resourceHexes.size() == 18;
 		assert ports.size() == 9;
 		assert waterHexes.size() == 9;
 		assert desertHex != null;
@@ -271,7 +271,7 @@ public class Board {
 			}
 		}
 		
-//		assert sixAndEights.size() == 4; 
+		assert sixAndEights.size() == 4; 
 		
 		for(ResourceHex hex1 : sixAndEights) {
 			HexLocation hex1Loc = hex1.getLocation();
