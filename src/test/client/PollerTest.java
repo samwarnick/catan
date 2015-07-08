f1bb4b89eb225e98a8072e91b2ce44f61fe9c71a
Zuwra <jordanjohnson16@gmail.com>
2015-07-08 09:28:05 -0600

    Junits for cc, poller, and tradeRatio, still needs a bit of work...

---
+++ src/test/client/PollerTest.java
@@ -0,0 +1,61 @@
+package test.client;
+
+import static org.junit.Assert.*;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.junit.Test;
+
+import server.ServerException;
+import shared.communication.input.GamesCreateInput;
+import shared.communication.input.GamesJoinInput;
+import shared.communication.input.GamesLoadInput;
+import shared.definitions.CatanColor;
+import shared.model.GameModel;
+import shared.model.player.Color;
+import client.controller.Controller;
+import client.poller.Poller;
+import client.poller.UpdateGame;
+import client.proxy.ProxyServer;
+
+public class PollerTest {
+	boolean changed = false;
+	ProxyServer proxy;
+	Controller controller;
+	
+	@Test
+	public void testPost() {
+		controller = new Controller(0);
+		proxy = controller.getProxyServer();
+		GamesCreateInput createGame = new GamesCreateInput("game1", false, false, false);
+		GamesJoinInput joinGame = new GamesJoinInput(0, Color.BLUE);
+		try {
+			proxy.createGame(createGame);
+			proxy.joinGame(joinGame);
+		} catch (ServerException e) {
+			assertTrue(false);
+		}
+
+		Timer timer = new Timer();
+		timer.schedule(checkChange(), 60001);
+		//change game via proxyServer
+		//wait 60000
+		//check if the change happened here too
+	}
+	
+	private TimerTask checkChange()
+	{
+		GamesLoadInput loadGame = new GamesLoadInput("game1");
+		try {
+			GameModel game = proxy.loadGame(loadGame); // GameModel game = controller.getGameModelFacade().getGameModel();
+			assertEquals(game.getPlayers().get(0).getColor(), CatanColor.BLUE);
+		} catch (ServerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
+
+}
---
+++ src/test/shared/model/TradeRatioTest.java
@@ -0,0 +1,32 @@
+package test.shared.model;
+
+import org.junit.Test;
+
+import shared.definitions.ResourceType;
+import shared.model.ratios.TradeRatio;
+import static org.junit.Assert.*;
+
+
+public class TradeRatioTest {
+	
+	@Test
+	public void testSetRatio() {
+		TradeRatio trade = new TradeRatio(ResourceType.BRICK);
+		try {
+			trade.setRatio(2);
+			assertEquals(trade.getRatio(), 2);
+			trade.setRatio(1);
+			assertTrue(false);
+		} catch (Exception e) {
+			
+		}
+		try {
+			trade.setRatio(5);
+			assertTrue(false);
+		} catch (Exception e) {
+
+		}
+		
+	}
+
+}
