2bf78f11ec0cd85526feaf7ce5e11a85a2b62ff4
Zuwra <jordanjohnson16@gmail.com>
2015-07-13 09:11:47 -0600

    change to gamesJoinInput

--- src/shared/communication/input/GamesJoinInput.java
+++ src/shared/communication/input/GamesJoinInput.java
@@ -1,6 +1,7 @@
 package shared.communication.input;
 
-import shared.model.player.Color;
+import shared.definitions.CatanColor;
+
 
 /**
  * This class contains the gamesJoin method name, the gameID and the Color for the new player.
@@ -10,9 +11,9 @@ import shared.model.player.Color;
 public class GamesJoinInput extends Input {
 
 	private int id;
-	private Color color;
+	private CatanColor color;
 	
-	public GamesJoinInput(int gameID, Color color) {
+	public GamesJoinInput(int gameID, CatanColor color) {
 		super("/games/join");
 		this.id = gameID;
 		this.color = color;
