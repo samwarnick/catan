87756df30a5a84201575274f7b6fe2b3604e9d5b
Matt0911 <Mattmanhardt@Gmail.com>
2015-07-19 23:18:42 -0600

    get Trade ratios for player

--- src/shared/model/board/PortHex.java
+++ src/shared/model/board/PortHex.java
@@ -1,8 +1,13 @@
 package shared.model.board;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import shared.definitions.PortType;
 import shared.locations.EdgeDirection;
 import shared.locations.HexLocation;
+import shared.locations.VertexDirection;
+import shared.locations.VertexLocation;
 
 public class PortHex extends WaterHex {
 
@@ -41,5 +46,61 @@ public class PortHex extends WaterHex {
 		this.orientation = orientation;
 	}
 	
+	public List<VertexLocation> getVertices() {
+		ArrayList<VertexLocation> vertices = new ArrayList<VertexLocation>();
+		VertexLocation loc;
+		switch (orientation) {
+		case North:
+			loc = new VertexLocation(getLocation(), VertexDirection.NorthWest);
+			vertices.add(loc);
+			vertices.addAll(loc.getAmbiguousVertices());
+			loc = new VertexLocation(getLocation(), VertexDirection.NorthEast);
+			vertices.add(loc);
+			vertices.addAll(loc.getAmbiguousVertices());
+			return vertices;
+		case NorthEast:
+			loc = new VertexLocation(getLocation(), VertexDirection.NorthEast);
+			vertices.add(loc);
+			vertices.addAll(loc.getAmbiguousVertices());
+			loc = new VertexLocation(getLocation().getNeighborLoc(EdgeDirection.SouthEast), VertexDirection.NorthWest);
+			vertices.add(loc);
+			vertices.addAll(loc.getAmbiguousVertices());
+			return vertices;
+		case NorthWest:
+			loc = new VertexLocation(getLocation(), VertexDirection.NorthWest);
+			vertices.add(loc);
+			vertices.addAll(loc.getAmbiguousVertices());
+			loc = new VertexLocation(getLocation().getNeighborLoc(EdgeDirection.SouthWest), VertexDirection.NorthEast);
+			vertices.add(loc);
+			vertices.addAll(loc.getAmbiguousVertices());
+			return vertices;
+		case South:
+			loc = new VertexLocation(getLocation().getNeighborLoc(EdgeDirection.South), VertexDirection.NorthEast);
+			vertices.add(loc);
+			vertices.addAll(loc.getAmbiguousVertices());
+			loc = new VertexLocation(getLocation().getNeighborLoc(EdgeDirection.South), VertexDirection.NorthWest);
+			vertices.add(loc);
+			vertices.addAll(loc.getAmbiguousVertices());
+			return vertices;
+		case SouthEast:
+			loc = new VertexLocation(getLocation().getNeighborLoc(EdgeDirection.South), VertexDirection.NorthEast);
+			vertices.add(loc);
+			vertices.addAll(loc.getAmbiguousVertices());
+			loc = new VertexLocation(getLocation().getNeighborLoc(EdgeDirection.SouthEast), VertexDirection.NorthWest);
+			vertices.add(loc);
+			vertices.addAll(loc.getAmbiguousVertices());
+			return vertices;
+		case SouthWest:
+			loc = new VertexLocation(getLocation().getNeighborLoc(EdgeDirection.SouthWest), VertexDirection.NorthEast);
+			vertices.add(loc);
+			vertices.addAll(loc.getAmbiguousVertices());
+			loc = new VertexLocation(getLocation().getNeighborLoc(EdgeDirection.South), VertexDirection.NorthWest);
+			vertices.add(loc);
+			vertices.addAll(loc.getAmbiguousVertices());
+			return vertices;
+		default:
+			return null;
+		}
+	}
 	
 }
