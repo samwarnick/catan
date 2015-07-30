6471a2e538fbefe7679bb52cd2b0fa4e6acb8aad
ihartung <isaac.hartung@gmail.com>
2015-07-30 13:32:43 -0600

    asD

--- src/shared/model/GameModel.java
+++ src/shared/model/GameModel.java
@@ -24,6 +24,7 @@ import shared.model.board.Vertex;
 import shared.model.player.ActivePlayerFacade;
 import shared.model.player.InactivePlayerFacade;
 import shared.model.player.Player;
+import shared.model.player.VictoryPoints;
 
 /**
  * The model for the game
@@ -67,6 +68,7 @@ public class GameModel {
 		}
 		if (!tooManyPlayers){
 			player.setPlayerID(indexToAdd);
+			player.setVictoryPoints(new VictoryPoints(0, 0));
 			players.add(indexToAdd, player);
 			players.remove(4);
 		}
