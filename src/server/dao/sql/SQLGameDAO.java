eb9850684a79ee9dd056b5c00e4b6e1460ca8570
ihartung <isaac.hartung@gmail.com>
2015-08-05 12:59:36 -0600

    awesome changes

--- src/server/dao/sql/SQLGameDAO.java
+++ src/server/dao/sql/SQLGameDAO.java
@@ -1,5 +1,13 @@
 package server.dao.sql;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.sql.Blob;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import client.data.GameInfo;
@@ -12,24 +20,109 @@ public class SQLGameDAO implements IGameDAO {
 	private Database database;
 	
 	public SQLGameDAO(Database database) {
-		this.database = database;
 	}
 
 	@Override
 	public void addGameModel(GameModel model) {
-		// TODO Auto-generated method stub
-
+		String query = "insert into GameModels (GameModel) values ( ?)";
+		PreparedStatement stmt = null;
+		ResultSet keyRS = null;		
+		try {
+			stmt = database.getConnection().prepareStatement(query);
+			Blob blob = database.getConnection().createBlob();
+			ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+			ObjectOutputStream objectStream = new ObjectOutputStream(byteStream);
+			objectStream.writeObject(model);
+			blob.setBytes(0, byteStream.toByteArray());
+			stmt.setBlob(1, blob);
+			if (stmt.executeUpdate() != 1) {
+				throw new DatabaseException("Could not insert user");
+			}
+		}
+		catch (SQLException e) {
+			try {
+				throw new DatabaseException("Could not insert user", e);
+			} catch (DatabaseException e1) {
+				e1.printStackTrace();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (DatabaseException e) {
+			e.printStackTrace();
+		}
+		finally {
+			Database.safeClose(stmt);
+			Database.safeClose(keyRS);
+		}
 	}
 
 	@Override
 	public void addGameInfo(GameInfo info) {
-		// TODO Auto-generated method stub
+		addCommands();
+		String query = "insert into GameInfos (GameInfo) values ( ?)";
+		PreparedStatement stmt = null;
+		ResultSet keyRS = null;		
+		try {
+			stmt = database.getConnection().prepareStatement(query);
+			Blob blob = database.getConnection().createBlob();
+			ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+			ObjectOutputStream objectStream = new ObjectOutputStream(byteStream);
+			objectStream.writeObject(info);
+			blob.setBytes(0, byteStream.toByteArray());
+			stmt.setBlob(1, blob);
+			if (stmt.executeUpdate() != 1) {
+				throw new DatabaseException("Could not insert user");
+			}
+		}
+		catch (SQLException e) {
+			try {
+				throw new DatabaseException("Could not insert user", e);
+			} catch (DatabaseException e1) {
+				e1.printStackTrace();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (DatabaseException e) {
+			e.printStackTrace();
+		}
+		finally {
+			Database.safeClose(stmt);
+			Database.safeClose(keyRS);
+		}
 
 	}
 
-	@Override
-	public void addCommands() {
-		// TODO Auto-generated method stub
+	private void addCommands() {
+		String query = "insert into Commands (Command) values ( ?)";
+		PreparedStatement stmt = null;
+		ResultSet keyRS = null;		
+		try {
+			stmt = database.getConnection().prepareStatement(query);
+			Blob blob = database.getConnection().createBlob();
+			ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+			ObjectOutputStream objectStream = new ObjectOutputStream(byteStream);
+			objectStream.writeObject(new ArrayList<MoveCommand>());
+			blob.setBytes(0, byteStream.toByteArray());
+			stmt.setBlob(1, blob);
+			if (stmt.executeUpdate() != 1) {
+				throw new DatabaseException("Could not insert user");
+			}
+		}
+		catch (SQLException e) {
+			try {
+				throw new DatabaseException("Could not insert user", e);
+			} catch (DatabaseException e1) {
+				e1.printStackTrace();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (DatabaseException e) {
+			e.printStackTrace();
+		}
+		finally {
+			Database.safeClose(stmt);
+			Database.safeClose(keyRS);
+		}
 
 	}
 
