710cc62a708a91de749c1ab307336029f762e063
Matt0911 <Mattmanhardt@Gmail.com>
2015-08-05 11:54:53 -0600

    dao

---
+++ src/server/dao/sql/SQLGameDAO.java
@@ -0,0 +1,66 @@
+package server.dao.sql;
+
+import java.util.List;
+
+import client.data.GameInfo;
+import server.commands.move.MoveCommand;
+import server.dao.IGameDAO;
+import shared.model.GameModel;
+
+public class SQLGameDAO implements IGameDAO {
+
+	@Override
+	public void addGameModel(GameModel model) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addGameInfo(GameInfo info) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addCommands() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateGameModel(GameModel model) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateGameInfo(GameInfo info) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateCommands(int gameID, List<MoveCommand> commands) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<GameModel> getAllGameModels() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<GameInfo> getAllGameInfos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<MoveCommand> getCommands(int gameID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
