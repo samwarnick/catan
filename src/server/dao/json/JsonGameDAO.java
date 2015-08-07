cb85a55aa501edbd8c0ee3ebf3ab64bdf32bb9d5
samwarnick <sam.t.warnick@gmail.com>
2015-08-07 13:06:32 -0600

    son adds and update

--- src/server/dao/json/JsonGameDAO.java
+++ src/server/dao/json/JsonGameDAO.java
@@ -2,20 +2,24 @@ package server.dao.json;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import client.data.GameInfo;
+import server.GameHub;
 import server.commands.move.MoveCommand;
 import server.dao.IGameDAO;
 import shared.model.GameModel;
 
 import org.apache.commons.io.FileUtils;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonGameDAO implements IGameDAO {
 	
 	private int commandLimit;
+	final String path = "Persistance" + File.separator + "Json" + File.separator;
 	
 	public JsonGameDAO(int commandLimit) {
 		this.commandLimit = commandLimit;
@@ -23,8 +27,7 @@ public class JsonGameDAO implements IGameDAO {
 
 	@Override
 	public void addGameModel(GameModel model) {
-		// TODO Auto-generated method stub
-		File dir = new File("Persistance" + File.separator + "Json" + File.separator + model.getGameID());
+		File dir = new File(path + model.getGameID());
 		if (!dir.exists()) {
 			if (!dir.mkdir()) {
 				System.out.println("directory failed to be created");
@@ -36,7 +39,7 @@ public class JsonGameDAO implements IGameDAO {
 		
 		ObjectMapper mapper = new ObjectMapper();
 		try {
-			mapper.writeValue(new File("Persistance/Json/model.json"), model);
+			mapper.writeValue(new File(path + model.getGameID() + File.separator + "model.json"), model);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -44,8 +47,7 @@ public class JsonGameDAO implements IGameDAO {
 
 	@Override
 	public void addGameInfo(GameInfo info) {
-		// TODO Auto-generated method stub
-		File dir = new File("Persistance" + File.separator + "Json" + File.separator + info.getId());
+		File dir = new File(path + info.getId());
 		if (!dir.exists()) {
 			if (!dir.mkdir()) {
 				System.out.println("directory failed to be created");
@@ -57,7 +59,7 @@ public class JsonGameDAO implements IGameDAO {
 		
 		ObjectMapper mapper = new ObjectMapper();
 		try {
-			mapper.writeValue(new File("Persistance/Json/info.json"), info);
+			mapper.writeValue(new File(path + info.getId() + File.separator + "info.json"), info);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -67,8 +69,24 @@ public class JsonGameDAO implements IGameDAO {
 	@Override
 	public void addCommand(int gameID, MoveCommand command) {
 		ObjectMapper mapper = new ObjectMapper();
+		File file = new File(path + gameID + File.separator + "commands.json");
+		List<MoveCommand> commands = null;
 		try {
-			mapper.writeValue(new File("Persistance/Json/commands.json"), model);
+			if (file.exists()) {
+				commands = mapper.readValue(file, new TypeReference<List<MoveCommand>>(){});
+				if (commands != null && commands.size() < commandLimit - 1) {
+					commands.add(command);
+					mapper.writeValue(file, commands);
+				}
+				else {
+					mapper.writeValue(new File(path + gameID + File.separator + "model.json"), GameHub.getInstance().getModel(gameID));
+					mapper.writeValue(file, new ArrayList<MoveCommand>());
+				}
+			}
+			else {
+				mapper.writeValue(file, new ArrayList<MoveCommand>());
+			}
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -76,14 +94,34 @@ public class JsonGameDAO implements IGameDAO {
 	
 	@Override
 	public void updateGameModel(GameModel model) {
-		// TODO Auto-generated method stub
-
+		File file = new File(path + model.getGameID() + File.separator + "model.json");
+		if (file.exists()) {
+			ObjectMapper mapper = new ObjectMapper();
+			try {
+				mapper.writeValue(file, model);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		else {
+			System.out.println("game did not exist in persistance storage");
+		}
 	}
 
 	@Override
 	public void updateGameInfo(GameInfo info) {
-		// TODO Auto-generated method stub
-
+		File file = new File(path + info.getId() + File.separator + "info.json");
+		if (file.exists()) {
+			ObjectMapper mapper = new ObjectMapper();
+			try {
+				mapper.writeValue(file, info);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		else {
+			System.out.println("info did not exist in persistance storage");
+		}
 	}
 
 	@Override
