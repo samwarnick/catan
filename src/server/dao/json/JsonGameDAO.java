e25997784c49cbf67dab41cb1928f0c8b80a3b82
samwarnick <sam.t.warnick@gmail.com>
2015-08-07 12:32:02 -0600

    son

--- src/server/dao/json/JsonGameDAO.java
+++ src/server/dao/json/JsonGameDAO.java
@@ -1,5 +1,7 @@
 package server.dao.json;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import client.data.GameInfo;
@@ -7,24 +9,69 @@ import server.commands.move.MoveCommand;
 import server.dao.IGameDAO;
 import shared.model.GameModel;
 
+import org.apache.commons.io.FileUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class JsonGameDAO implements IGameDAO {
+	
+	private int commandLimit;
+	
+	public JsonGameDAO(int commandLimit) {
+		this.commandLimit = commandLimit;
+	}
 
 	@Override
 	public void addGameModel(GameModel model) {
 		// TODO Auto-generated method stub
-
+		File dir = new File("Persistance" + File.separator + "Json" + File.separator + model.getGameID());
+		if (!dir.exists()) {
+			if (!dir.mkdir()) {
+				System.out.println("directory failed to be created");
+			}
+		}
+		else {
+			System.out.println("directory already exists");
+		}
+		
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			mapper.writeValue(new File("Persistance/Json/model.json"), model);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void addGameInfo(GameInfo info) {
 		// TODO Auto-generated method stub
+		File dir = new File("Persistance" + File.separator + "Json" + File.separator + info.getId());
+		if (!dir.exists()) {
+			if (!dir.mkdir()) {
+				System.out.println("directory failed to be created");
+			}
+		}
+		else {
+			System.out.println("directory already exists");
+		}
+		
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			mapper.writeValue(new File("Persistance/Json/info.json"), info);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
 	@Override
 	public void addCommand(int gameID, MoveCommand command) {
-		// TODO Auto-generated method stub
-		
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			mapper.writeValue(new File("Persistance/Json/commands.json"), model);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
--- src/server/factories/JsonFactory.java
+++ src/server/factories/JsonFactory.java
@@ -2,17 +2,19 @@ package server.factories;
 
 import server.dao.IGameDAO;
 import server.dao.IUserDAO;
+import server.dao.json.JsonGameDAO;
+import server.dao.json.JsonUserDAO;
 
 public class JsonFactory implements AbstractFactory{
 
 	@Override
 	public IGameDAO makeGameDAO(int commandLimit) {
-		return null;
+		return new JsonGameDAO(commandLimit);
 	}
 
 	@Override
 	public IUserDAO makeUserDAO() {
-		return null;
+		return new JsonUserDAO();
 	}
 
 }
